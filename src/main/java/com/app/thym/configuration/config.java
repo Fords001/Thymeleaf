9e4fcaf082781d32ac4687ea5a9a0bc877436f7d
Fords001 <2312445@gmail.com>
2021-03-31 02:12:42 +0300

    2 controller and html connection

--- src/main/java/com/app/thym/configuration/config.java
+++ src/main/java/com/app/thym/configuration/config.java
@@ -1,17 +1,33 @@
 package com.app.thym.configuration;
 
+import com.app.thym.dao.PersonRepository;
+import com.app.thym.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Controller
 public class config {
+
+    @Autowired
+    private PersonRepository personrepo;
 
     @RequestMapping (value="/")
     public String mainMenu(/*Model model*/){
         //model.addAttribute("som" , "Jyst ike thar");
         return "main-menu";
+    }
+// show the table
+    @GetMapping(value="/list")
+    public String list1(Model model1){
+        List<Person> person1=personrepo.findAll();
+        model1.addAttribute("person",person1);
+        return "personpage";
+
     }
 }
