f30ce3c32f6411d74aeacfdaf8a716770edfef26
Fords001 <2312445@gmail.com>
2021-04-01 04:21:15 +0300

    4 create page for adding elements

--- src/main/java/com/app/thym/configuration/config.java
+++ src/main/java/com/app/thym/configuration/config.java
@@ -5,9 +5,7 @@ import com.app.thym.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,17 +15,30 @@ public class config {
     @Autowired
     private PersonRepository personrepo;
 
-    @RequestMapping (value="/")
-    public String mainMenu(/*Model model*/){
+    @RequestMapping(value = "/")
+    public String mainMenu(/*Model model*/) {
         //model.addAttribute("som" , "Jyst ike thar");
         return "main-menu";
     }
-// show the table
-    @GetMapping(value="/list")
-    public String list1(Model model1){
-        List<Person> person1=personrepo.findAll();
-        model1.addAttribute("person",person1);
-        return "personpage";
 
+    // show the table
+    @GetMapping(value = "/list")
+    public String list1(Model model1) {
+        List<Person> person1 = personrepo.findAll();
+        model1.addAttribute("person", person1);
+        return "personpage";
     }
+
+    @GetMapping(value = "/addlist")
+    public String linktothepage(Model model2) {
+    Person person2=new Person();
+    model2.addAttribute("person",person2);
+    return "createperson";
+    }
+    @PostMapping(value="/save")
+    public String saveperson (@ModelAttribute("person") Person person3){
+        personrepo.save(person3);
+        return "redirect:/list";
+    }
+
 }
