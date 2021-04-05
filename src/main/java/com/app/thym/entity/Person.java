78f51baac009ab34f85e98323f84a930b0300aae
Fords001 <2312445@gmail.com>
2021-04-05 18:22:36 +0300

    8 validation and css update

--- src/main/java/com/app/thym/entity/Person.java
+++ src/main/java/com/app/thym/entity/Person.java
@@ -1,13 +1,18 @@
 package com.app.thym.entity;
 
 
+import com.app.thym.validator.ExtendedEmailValidator;
 import com.sun.istack.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
+import javax.validation.ConstraintValidator;
+import javax.validation.Payload;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.lang.annotation.Annotation;
 
 @Entity
 @Data
@@ -16,14 +21,14 @@ import javax.validation.constraints.Size;
 @ToString
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Table(name="`person_table`")
-public class Person {
+public class Person  {
 
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="id")
     int id;
-    
+
     @Column(name="firstname")
     @NotBlank(message = "Firstname should not be empty")
     @Size(min=2 ,max=30 , message="Firstname should be between 2 and 30 characters")
@@ -37,5 +42,7 @@ public class Person {
     @Column(name="email")
     @NotBlank(message = "Email should not be empty")
     @Size(min=2 ,max=30 , message="Email should be between 2 and 30 characters")
-    String email;
+    @ExtendedEmailValidator
+    String email ;
+
 }
